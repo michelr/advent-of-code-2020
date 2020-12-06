d9b2cc654729011578820635886e4e05f12efb6d
Michel Radosavljevic <mr@michel.se>
2020-12-06 16:52:18 +0100

    change struct member name

--- assignment2/src/main.rs
+++ assignment2/src/main.rs
@@ -5,20 +5,20 @@ use std::fs;
 struct Line {
     min: i32,
     max: i32,
-    digit: char,
+    character: char,
     password: String,
 }
 
 fn main() {
     let in_range = |line: Line| {
-        let count = line.password.matches(line.digit).count() as i32;
+        let count = line.password.matches(line.character).count() as i32;
         line.min <= count && count <= line.max
     };
 
     let in_position = |line: Line| {
         let passsword_chars = line.password.chars().collect::<Vec<char>>();
-        let is_first = passsword_chars[(line.min - 1) as usize] == line.digit;
-        let is_second = passsword_chars[(line.max - 1) as usize] == line.digit;
+        let is_first = passsword_chars[(line.min - 1) as usize] == line.character;
+        let is_second = passsword_chars[(line.max - 1) as usize] == line.character;
         is_first != is_second
     };
 
@@ -42,14 +42,14 @@ fn valid_passwords(f: &dyn Fn(Line) -> bool) -> usize {
 fn parse(row: &str) -> Line {
     lazy_static! {
         static ref RE: Regex =
-            Regex::new(r"^(?P<min>\d+)-(?P<max>\d+) (?P<digit>[a-z]): (?P<password>[a-z]+)$")
+            Regex::new(r"^(?P<min>\d+)-(?P<max>\d+) (?P<char>[a-z]): (?P<password>[a-z]+)$")
                 .unwrap();
     }
     match RE.captures(&row) {
         Some(cap) => Line {
             min: cap.name("min").unwrap().as_str().parse::<i32>().unwrap(),
             max: cap.name("max").unwrap().as_str().parse::<i32>().unwrap(),
-            digit: cap.name("digit").unwrap().as_str().chars().next().unwrap(),
+            character: cap.name("char").unwrap().as_str().chars().next().unwrap(),
             password: cap.name("password").unwrap().as_str().to_string(),
         },
         None => panic!("Regex capture problem"),
